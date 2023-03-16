32257ae50e27897b80376ff7193699d519a9dc55
DinhVan <dinhvanvtpt123@gmail./com>
2023-03-16 22:44:43 +0700

    vu: create app 16032023

---
+++ src/components/Header.js
@@ -0,0 +1,55 @@
+import React from "react";
+import { NavLink, Link } from "react-router-dom";
+import { BsSearch } from 'react-icons/bs';
+
+export const Header = () => {
+  return (
+    <>
+      <header className="header-top-strip py-3">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-6">
+              <p className="text-white mb-0">
+                Free Shipping Over $100 & Free Returns
+              </p>
+            </div>
+            <div className="col-6">
+              <p className="text-end text-white mb-0">
+                Hotline:
+                <a className="text-white" href="tel:+91 8264954234">
+                  +91 8264954234
+                </a>
+              </p>
+            </div>
+          </div>
+        </div>
+      </header>
+      <header className="header-upper py-3">
+        <div className="container-xxl">
+          <div className="row align-items-center">
+            <div className="col-2">
+              <h2>
+                <Link className="text-white">Dev</Link>
+              </h2>
+            </div>
+            <div className="col-5">
+              <div className="input-group">
+                <input
+                  type="text"
+                  className="form-control py-2"
+                  placeholder="Search Product Here..."
+                  aria-label="Search Product Here..."
+                  aria-describedby="basic-addon2"
+                />
+                <span className="input-group-text p-3" id="basic-addon2">
+                  <BsSearch className="fs-6"/>
+                </span>
+              </div>
+            </div>
+            <div className="col-5"></div>
+          </div>
+        </div>
+      </header>
+    </>
+  );
+};
